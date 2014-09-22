adbe5546b13cc81e1aaa66f331877cdd5bcdd161
kenwilcox <wilcoxk@gmail.com>
2014-09-21 19:50:52 -0600

    UI Setup

---
+++ 99RedBalloons/Balloon.swift
@@ -0,0 +1,15 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Kenneth Wilcox on 9/21/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+  var number = 0
+  var image = UIImage(CGImage:nil)
+}
--- 99RedBalloons/ViewController.swift
+++ 99RedBalloons/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var balloonLabel: UILabel!
+  @IBOutlet weak var imageView: UIImageView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func nextBallonPressed(sender: AnyObject) {
+    println("Pressed")
+  }
 
 }
 
